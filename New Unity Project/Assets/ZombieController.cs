b3976f7d316c4c25c5e0a072660f8657e9edb06a
LO XIANG HUI <loxh-wm20@student.tarc.edu.my>
2021-08-09 13:01:33 +0800

    practical8

--- New Unity Project/Assets/ZombieController.cs
+++ New Unity Project/Assets/ZombieController.cs
@@ -2,39 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieSpawner: MonoBehaviour
+public class ZombieController : MonoBehaviour
 {
-    public GameObject enemy;
-    float randX;
-    float randY;
-    //Zombie location that spawn randomly
-    float xWidth;
-    float yHeight;
-
-    Vector2 wheretoSpawn;
-    public float spawnRate = 2f;
-    float nextSpawn = 0.0f;
-    static int maxEnemy = 4;
-    static float enemyCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(enemyCount<maxEnemy)
-        {
-            if(Time.time>nextSpawn)
-            {
-                nextSpawn = Time.time + spawnRate;
-                xWidth = (Screen.width - 32) / 100;
-                yHeight = (Screen.width - 32) / 100;
-
-                randX = Random.Range(-xWidth, xWidth);
-                randY = Random.Range(-yHeight, yHeight);
-                wheretoSpawn = new Vector2(randX, randY);
-                Instantiate(enemy, wheretoSpawn, Quaternion.identity);
-                enemyCount++;
-
-            }
-        }
+        
     }
 }
--- New Unity Project/Assets/ZombieSpawner.cs
+++ New Unity Project/Assets/ZombieSpawner.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieController : MonoBehaviour
+public class ZombieSpawner: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject enemy;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    float randX;
+    float randY;
+    //Zombie location that spawn randomly
+    float xWidth;
+    float yHeight;
+
+    Vector2 wheretoSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    static int maxEnemy = 6;
+    static float enemyCount = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemyCount<maxEnemy)
+        {
+            if(Time.time>nextSpawn)
+            {
+                nextSpawn = Time.time + spawnRate;
+                xWidth = (Screen.width - 32) / 100;
+                yHeight = (Screen.width - 32) / 100;
+
+                randX = Random.Range(-xWidth, xWidth);
+                randY = Random.Range(-yHeight, yHeight);
+                wheretoSpawn = new Vector2(randX, randY);
+                if (Random.Range(0,2)==0)
+                Instantiate(enemy, wheretoSpawn, Quaternion.identity);
+                else if (Random.Range(0, 2) == 1)
+                    Instantiate(enemy2, wheretoSpawn, Quaternion.identity);
+                else 
+                    Instantiate(enemy3, wheretoSpawn, Quaternion.identity);
+                enemyCount++;
+
+            }
+        }
     }
 }
